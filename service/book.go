85a909c98edd30e3ee32c6c45477ce64c21795c8
盛辉 <847713844@qq.com>
2017-06-28 17:24:27 +0800

    update

--- service/book.go
+++ service/book.go
@@ -10,16 +10,16 @@ import (
 )
 
 type Book struct {
-	ID          int       `db:"id"`
-	Title       string    `db:"title"`
-	SubTitle    string    `db:"subtitle"`
-	Author      string    `db:"author"`
-	Version     string    `db:"version"`
-	Price       string    `db:"price"`
-	Publisher   string    `db:"publisher"`
-	PublishDate string    `db:"publish_date"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
+	ID          int       `db:"id" json:"id"`
+	Title       string    `db:"title" json:"title"`
+	SubTitle    string    `db:"subtitle" json:"subtitle"`
+	Author      string    `db:"author" json:"author"`
+	Version     string    `db:"version" json:"version"`
+	Price       string    `db:"price" json:"price"`
+	Publisher   string    `db:"publisher" json:"publisher"`
+	PublishDate string    `db:"publish_date" json:"publish_date"`
+	CreatedAt   time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at" json:"updated_at"`
 }
 
 func GetBookById(id int) (yiigo.X, error) {
