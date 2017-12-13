a1834df00663240c16c00017611a1dc75bfdb6f4
盛辉 <shenghui@lanjingren.com>
2017-12-13 22:00:22 +0800

    update

--- middlewares/recovery.go
+++ middlewares/recovery.go
@@ -13,7 +13,7 @@ func Recovery() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
-				yiigo.Logger.Sugar().Error(err, fmt.Sprintf("\n%s", string(debug.Stack())))
+				yiigo.Logger.Error(fmt.Sprintf("%v\n%v", err, string(debug.Stack())))
 				yiigo.Error(c, 500, "Internal server error")
 				c.Abort()
 
