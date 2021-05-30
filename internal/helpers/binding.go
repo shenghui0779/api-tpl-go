bc6e39718e60d2552be906c75e9f838f49df8d24
shenghui <67535398+shenghui0779@users.noreply.github.com>
2021-05-30 23:32:17 +0800

    Update binding.go

--- internal/helpers/binding.go
+++ internal/helpers/binding.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/shenghui0779/yiigo"
@@ -10,6 +11,8 @@ import (
 var validator = yiigo.NewValidator()
 
 func BindJSON(r *http.Request, obj interface{}) error {
+	defer io.Copy(io.Discard, r.Body)
+	
 	if err := json.NewDecoder(r.Body).Decode(obj); err != nil {
 		return err
 	}
