4ba8b3ae351fbcbb3c0eff57f5bbd88c74e0ab87
shenghui <shenghui0779@gmail.com>
2022-10-31 16:19:11 +0800

    update config

--- pkg/config/db.go
+++ pkg/config/db.go
@@ -20,25 +20,25 @@ func DB() *yiigo.DBConfig {
 	}
 
 	if v := os.Getenv("DB_MAX_OPEN_CONNS"); len(v) != 0 {
-		if i, err := strconv.Atoi(v); err != nil && i > 0 {
+		if i, err := strconv.Atoi(v); err == nil && i > 0 {
 			cfg.Options.MaxOpenConns = i
 		}
 	}
 
 	if v := os.Getenv("DB_MAX_IDLE_CONNS"); len(v) != 0 {
-		if i, err := strconv.Atoi(v); err != nil && i > 0 {
+		if i, err := strconv.Atoi(v); err == nil && i > 0 {
 			cfg.Options.MaxIdleConns = i
 		}
 	}
 
 	if v := os.Getenv("DB_CONN_MAX_LIFE_TIME"); len(v) != 0 {
-		if i, err := strconv.Atoi(v); err != nil && i > 0 {
+		if i, err := strconv.Atoi(v); err == nil && i > 0 {
 			cfg.Options.ConnMaxLifetime = time.Duration(i) * time.Second
 		}
 	}
 
 	if v := os.Getenv("DB_CONN_MAX_IDLE_TIME"); len(v) != 0 {
-		if i, err := strconv.Atoi(v); err != nil && i > 0 {
+		if i, err := strconv.Atoi(v); err == nil && i > 0 {
 			cfg.Options.ConnMaxIdleTime = time.Duration(i) * time.Second
 		}
 	}
