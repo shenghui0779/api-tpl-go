8b53c841b786ba3850331ba6ac94d5e928c01262
shenghui <shenghui0779@gmail.com>
2023-01-12 16:16:02 +0800

    更新 util.go

--- pkg/lib/util.go
+++ pkg/lib/util.go
@@ -1,13 +1,36 @@
 package lib
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/hex"
 	"io"
 	"net/http"
 	"regexp"
+	"runtime/debug"
 	"strings"
+
+	"tplgo/pkg/logger"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
 )
+
+// Recover recover panic
+func Recover(ctx context.Context) {
+	if err := recover(); err != nil {
+		logger.Err(ctx, "Server Panic",
+			zap.Any("error", err),
+			zap.ByteString("stack", debug.Stack()),
+		)
+	}
+}
+
+// CtxCopyWithReqID returns a new context with request_id from origin context.
+// Often used for goroutine.
+func CtxCopyWithReqID(ctx context.Context) context.Context {
+	return context.WithValue(context.Background(), middleware.RequestIDKey, middleware.GetReqID(ctx))
+}
 
 func Nonce() string {
 	nonce := make([]byte, 8)
